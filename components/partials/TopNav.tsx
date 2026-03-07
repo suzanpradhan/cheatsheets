8f3e943dd0ce9a7b8bacbb6decbab7e767d09f89
ushnika <ushnikashrestha22@gmail.com>
2026-03-07 11:59:57 +0545

    feat: added logout button and stopped user to go to snippet page without login

--- components/partials/TopNav.tsx
+++ components/partials/TopNav.tsx
@@ -1,13 +1,22 @@
 'use client';
 import { TextInput } from '@mantine/core';
-import { IconSearch, IconTerminal } from '@tabler/icons-react';
+import { IconLogout, IconSearch, IconTerminal } from '@tabler/icons-react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchQuery } from '@/store/store';
 import { RootState } from '@/store/store';
+import { signOut } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 
 export const TopNav = () => {
   const dispatch = useDispatch();
+  const router = useRouter();
   const query = useSelector((state: RootState) => state.search.query);
+
+  const handleSignOut = async () => {
+    await signOut({ redirect: false });
+    router.push('/login');
+  };
+
   return (
     <nav className="border-b border-slate-200 px-6 py-4 sticky top-0 z-20 bg-white">
       <div className="max-w-5xl mx-auto flex items-center justify-between">
@@ -25,10 +34,17 @@ export const TopNav = () => {
         <TextInput
           placeholder="Filter..."
           leftSection={<IconSearch size={16} />}
-          className="w-40 md:w-60 rounded-md"
+          className="w-40 md:w-80 rounded-md"
           value={query}
           onChange={(e) => dispatch(setSearchQuery(e.target.value))}
         />
+        <div
+          className="flex items-center gap-2 cursor-pointer"
+          onClick={handleSignOut}
+        >
+          <IconLogout />
+          <p>Logout</p>
+        </div>
       </div>
     </nav>
   );
--- middleware.ts
+++ middleware.ts
@@ -14,5 +14,5 @@ export default withAuth(
 );
 
 export const config = {
-  matcher: ['/'],
+  matcher: ['/snippets', '/snippets/:path*'],
 };
